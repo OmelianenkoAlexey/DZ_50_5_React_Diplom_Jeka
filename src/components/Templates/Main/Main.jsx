cb8d5088b6a41e2ce6f4a5133b62c851348f75ae
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-04-02 18:56:30 +0300

    3

--- src/components/Templates/Main/Main.jsx
+++ src/components/Templates/Main/Main.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import TrainingCard from './ComponentMain/TrainingCard';
-import { classes } from '../../../api';
+// import { classes } from '../../../api';
+// можно переименовывать переменные import
+import { classes as newClasses } from '../../../api';
 import './Main.css';
 
 export default function Trains() {
   const [trainingsData, setClasses] = useState([]);
-
+  // const info = '555';
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await classes.fetch();
+        const { data } = await newClasses.fetch();
+        // const { data } = await newClasses(info).fetch();
         setClasses(data);
       } catch (err) {
         console.log(err);
@@ -22,7 +25,7 @@ export default function Trains() {
   localStorage.setItem('searchLocal', newTrainings);
 
   return (
-      <div className='training_card_wrapper'>
+    <div className='training_card_wrapper'>
       {trainingsData.map((training) => (
         <TrainingCard
           trainingName={training.name}
